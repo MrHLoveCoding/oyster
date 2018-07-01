451238f8224638d94f306b33cc369c9460294d13
MrHLoveCoding <best_yet_to_come@sina.cn>
2018-07-01 09:10:46 +0000

    Create oys_types.h

---
+++ src/includes/oys_types.h
@@ -0,0 +1,28 @@
+#ifndef __OYS_TYPES_H__
+#define __OYS_TYPES_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+typedef uint8_t oys_u8_t;
+typedef uint16_t oys_u16_t;
+typedef uint32_t oys_u32_t;
+typedef uint64_t oys_u64_t;
+
+typedef uint32_t oys_be32_t;
+typedef uint16_t oys_be16_t;
+typedef uint64_t oys_be64_t;
+
+typedef int8_t oys_s8_t;
+typedef int16_t oys_s16_t;
+typedef int32_t oys_s32_t;
+typedef int64_t oys_s64_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
